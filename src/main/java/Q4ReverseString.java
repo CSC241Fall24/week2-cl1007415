1bcf67900b52c451f777c4b7916b8702a76556d6
cl1007415 <112582455+cl1007415@users.noreply.github.com>
2024-09-05 13:09:30 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -4,6 +4,16 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         // TODO: Implement the recursive function to reverse the string
-        return "";
+        int num = s.length();
+        String temp = "";
+        if(num <= 1)
+        {
+            return temp;
+        }else if(num > 1)
+        {
+            temp = reverse(s.substring(1)) + s.charAt(0);
+            num--;
+        }
+        return temp;
     }
 }
